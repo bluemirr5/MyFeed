0ad679faac9f4bd8829a6c8e60fb6f039bfd77f8
bluemirr5 <bluemirr5@bluemirr5-ubuntu.(none)>
2013-06-06 13:56:44 +0900

    a

--- src/main/java/com/nnggstory/feedfactory/model/UserDataModel.java
+++ src/main/java/com/nnggstory/feedfactory/model/UserDataModel.java
@@ -2,8 +2,10 @@ package com.nnggstory.feedfactory.model;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document
 public class UserDataModel {
-  public @Document String id = null;
-  public @Document String email = null;
-  public @Document String nickName = null;
+  public String id = null;
+  public String password = null;
+  public String email = null;
+  public String nickName = null;
 }
